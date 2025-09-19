d608eae6d72e0bba1e5a177d54d66e91c92eddc5
Ardily <ad2241@cam.ac.uk>
2025-09-20 00:39:58 +0100

    split data into sets

--- research/LSTM_training.py
+++ research/LSTM_training.py
@@ -6,4 +6,12 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 
 train_data = pd.read_csv('./data/train.csv')
-test_data = pd.read_csv('./data/test.csv')
+#test_data = pd.read_csv('./data/test.csv')
+
+train_load, val_load, test_load = train_data[0:48000], train_data[48000:64000], train_data[64000:80001]
+
+x_train, y_train = train_load[['A','B','C','D','E','F','G','H','I','J','K','L','M','N']].values, train_load[['Y1','Y2']].values
+x_val, y_val = val_load[['A','B','C','D','E','F','G','H','I','J','K','L','M','N']].values, val_load[['Y1','Y2']].values
+x_test, y_test = test_load[['A','B','C','D','E','F','G','H','I','J','K','L','M','N']].values, test_load[['Y1','Y2']].values
+
+DataLoader(train_data, batch_size=32, shuffle = True)
